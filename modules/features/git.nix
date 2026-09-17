9c09d2af14d0d85fd2af8ef6de1e3f2f525b43de
Stefan Visser <stefan.visser@apm-ecampus.de>
2026-09-17 11:50:34 +0200

    chore: Use wrapped git from nix-wrappers

--- modules/features/git.nix
+++ modules/features/git.nix
@@ -3,17 +3,16 @@
 {
   perSystem = { pkgs, ... }:
     {
-      packages.git = inputs.wrapper-modules.lib.wrapPackage {
+      packages.git = inputs.wrapper-modules.wrappers.git.wrap {
         inherit pkgs;
-        package = pkgs.git;
-        env = {
-          GIT_CONFIG_COUNT = "3";
-          GIT_CONFIG_KEY_0 = "init.defaultBranch";
-          GIT_CONFIG_VALUE_0 = "main";
-          GIT_CONFIG_KEY_1 = "pull.rebase";
-          GIT_CONFIG_VALUE_1 = "true";
-          GIT_CONFIG_KEY_2 = "push.autoSetupRemote";
-          GIT_CONFIG_VALUE_2 = "true";
+        settings = {
+          user = {
+            name = "Stefan Visser";
+            email = "stefan.visser@apm-ecampus.de";
+          };
+          init.defaultBranch = "main";
+          pull.rebase = true;
+          push.autoSetupRemote = true;
         };
       };
     };
