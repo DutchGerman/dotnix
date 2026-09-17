c10347c31353b228057be82ad7b4516e334ce355
Stefan Visser <stefan.visser@apm-ecampus.de>
2026-09-17 10:54:19 +0200

    chore: Configure keyring for VS Code

--- modules/features/vscode.nix
+++ modules/features/vscode.nix
@@ -1,8 +1,21 @@
-{ ... }:
+{ inputs, self, ... }:
 
 {
   flake.nixosModules.vscode = { pkgs, ... }:
     {
-      environment.systemPackages = [ pkgs.vscode ];
+      environment.systemPackages = [
+        self.packages.${pkgs.stdenv.hostPlatform.system}.vscode
+      ];
+    };
+
+  perSystem = { pkgs, ... }:
+    {
+      packages.vscode = inputs.wrapper-modules.lib.wrapPackage {
+        inherit pkgs;
+        package = pkgs.vscode;
+        flags = {
+          "--password-store" = "gnome-libsecret";
+        };
+      };
     };
 }
