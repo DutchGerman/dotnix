10acf5bc838a23d2d58c84d041ed6fac338a70cf
Stefan Visser <stefan.visser@apm-ecampus.de>
2026-09-25 10:32:53 +0200

    chore: Add python langauge support to vscode

--- modules/features/vscode.nix
+++ modules/features/vscode.nix
@@ -15,6 +15,7 @@
         package = pkgs.vscode-with-extensions.override {
           vscodeExtensions = [
             pkgs.vscode-extensions.jnoortheen.nix-ide
+            pkgs.vscode-extensions.ms-python.python
             pkgs.vscode-extensions.thenuprojectcontributors.vscode-nushell-lang
             pkgs.vscode-extensions.vue.volar
           ];
